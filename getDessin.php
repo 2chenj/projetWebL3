220ea2b3f150ffacd016852b5fbfc5ab194d5113
info121 <sebastien.patte@u-psud.fr>
2019-11-19 15:51:48 +0100

    fonction pour affichage d un bar chart

--- getDessin.php
+++ getDessin.php
@@ -1,10 +1,14 @@
 <?php 
         header('Content-Type: application/json');
 
-        $x = array(20,300);
-        $y = array(20,300);
-        $tab =array("forme" => (array("x"=>$x, "y"=>$y) ));
-
+        
+        $plein = 11;
+        $reduit = 8;
+        $sj = 2;
+        $sa = 1;
+        $tab =array("barre1" => (array("plein"=>$plein, "reduit"=>$reduit,"sj"=>$sj,"sa"=>$sa)),
+        			"barre2" => (array("plein"=>$plein+2, "reduit"=>$reduit+8,"sj"=>$sj+4,"sa"=>$sa+12)));
+		
         print(json_encode($tab));
 
 ?>
--- resultats.js
+++ resultats.js
@@ -1,22 +1,75 @@
+const width_canevas = 1000;
+const height_canevas = 1000;
+const nb_barre = 2;
+const width_barre = 50;				
+
+function line(x1, y1, x2, y2){
+	var c = document.getElementById("dessin");
+	var ctx = c.getContext("2d");
+	ctx.beginPath();
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+	ctx.stroke(); 
+}
+
+function printCarreHaut(posX, posY, width, heigth, color){
+	var c = document.getElementById("dessin");
+    var ctx = c.getContext("2d");
+	ctx.beginPath();
+   	ctx.rect(posX, (height_canevas/2)-posY-heigth, width, heigth);
+   	ctx.stroke();
+	ctx.fillStyle = color;
+	ctx.fill();
+
+}
+
+function printCarreBas(posX, posY, width, heigth, color){
+	var c = document.getElementById("dessin");
+    var ctx = c.getContext("2d");
+	ctx.beginPath();
+   	ctx.rect(posX, posY, width, heigth);
+   	ctx.stroke();
+	ctx.fillStyle = color;
+	ctx.fill();
+
+}
+
+function printBarre(decalage,width, plein, reduit, sj, sa){
+	var acc = 0;
+	printCarreHaut(decalage,0,width,plein,"red");
+	acc += plein;
+	printCarreHaut(decalage,acc,width,reduit, "green");
+	acc = (height_canevas/2);
+	printCarreBas(decalage,acc,width,sj, "blue");
+	acc += sj;
+	printCarreBas(decalage,acc,width,sa, "yellow");
+}
+
+function printPlusieuresBarres(width, data){
+	var decalage = 0;
+	for (var barre in data) {
+		        printBarre(
+	                decalage,
+	                width,
+			        data[barre]["plein"]*4,
+			        data[barre]["reduit"]*4,
+			        data[barre]["sj"]*4,
+	         		data[barre]["sa"]*4
+	         	);
+		decalage=decalage+70;
+	}
+}
+
 $.ajax({
 	type:'post',
 	url:"getDessin.php",
 	success:function(data){
 	        console.log(data)
-	        var c = document.getElementById("dessin");
-	        var ctx = c.getContext("2d");
-
-	                
-	             		ctx.beginPath();
-	                	ctx.rect(
-	                		data.forme.x[0] ,
-	                		data.forme.y[0] ,
-	                		data.forme.x[1] - data.forme.x[0] , 
-	                		data.forme.y[1] - data.forme.y[0]
-	                	);
-	                	ctx.stroke();
-	                	ctx.fillStyle = "red";
-						ctx.fill();
+	        
+			
+			
+			line(0,(height_canevas/2),height_canevas,(height_canevas/2));	        
+			printPlusieuresBarres(width_barre,data);	                
 	            
 	}
 })
