63305346a73607742abc57a2a07285b7b56f4f4b
info121 <sebastien.patte@u-psud.fr>
2019-12-03 14:36:06 +0100

    pas d'avertissement quand la pièce est déjà ds le panier

--- testTemps.js
+++ testTemps.js
@@ -11,12 +11,29 @@ function intToTime(nb){
 	}
 	return nbHeures+"h"+nbMin;
 }
+
+function estDans(nb, tab){
+	for (var i=0; i<tab.length; i++){
+		if(tab[i] == nb){
+			return true;
+		}
+	}
+	return false;
+}
 //on récupère le cookie panier
 var panier = eval(decodeURIComponent(document.cookie));
+// on remplit tabLignes avec les num de ligne des pieces réservées
+var i = 0;
+var tabLignes= [];
+for (var article in panier){
+	tabLignes[i] = panier[article]["lineReservation"]; 
+	i++;
+}
+
+
 //pour chaque article
 for(var article in panier){
 		console.log(panier[article]);
-	
 		//on récupère la ligne dans le csv de cet article
 		var ligne = panier[article]["lineReservation"];
 		console.log("ligne  => "+ligne);
@@ -31,7 +48,7 @@ for(var article in panier){
 				var jour1 = data['jour'];
 				var heure1 = timeToInt(data['heure']);
 				var ville1 = data['ville'];
-    					console.log("ville 1 = "+ville1);
+    			console.log("ville 1 = "+ville1);
 				console.log("heure1 = "+heure1);
 				for(var i=1; i<44;i++)
 				{
@@ -45,7 +62,7 @@ for(var article in panier){
 								var heure2 = timeToInt(data2['heure']);
 								var ville2 = data2['ville'];
 								
-								if(jour1==jour2 && i!=ligne){
+								if(jour1==jour2 && i!=ligne && !estDans(i,tabLignes)){
 									
 									$.ajax({
 										type:"POST",
@@ -54,34 +71,34 @@ for(var article in panier){
 										success:function(data3){
 											var elt = document.getElementById(i);
 											//comparer les horaires 
+												var diffTemps = 0;
+												if(heure2 >= heure1){
+													var fin1 = heure1 + 120; //on rajoute deux heures à la pièce pour avoir son heure de fin
+													diffTemps = heure2 - fin1;
+													if((diffTemps > 0) && (data3['t'] <= diffTemps)){
+														console.log("OK POUR : "+i);
+													}else{
+														console.log("PAS LE TEMPS POUR : "+i);
+														elt.style.backgroundColor = "red";
+														elt.rel = "tooltip";
+														elt.title = "Attention, vous avez une resprésentation réservée pour le même jour à "+intToTime(heure1)+" qui finit à "+intToTime(fin1)+ 
+																	" et il y a "+intToTime(data3['t'])+" min de trajet entre les deux salles vous n'aurez donc pas le temps d'assister aux 2 resprésentations";
+													}
+												}else{
+													var fin2 = heure2 + 120;
+													diffTemps = fin2 - heure1;
+													if((diffTemps > 0) && data3['t'] <= diffTemps){
+														console.log("OK POUR : "+i);
+													}else{
+														console.log("PAS LE TEMPS POUR : "+i);
+														elt.style.backgroundColor = "red";
+														elt.rel = "tooltip";
+														elt.title = "Attention, vous avez une resprésentation réservée pour le même jour à "+intToTime(heure2)+" qui finit à "+intToTime(fin2)+ 
+																	" et il y a "+intToTime(data3['t'])+" min de trajet entre les deux salles vous n'aurez donc pas le temps d'assister aux 2 resprésentations";
+													}
+												}
 											
-											var diffTemps = 0;
-											if(heure2 >= heure1){
-												var fin1 = heure1 + 120; //on rajoute deux heures à la pièce pour avoir son heure de fin
-												diffTemps = heure2 - fin1;
-												if((diffTemps > 0) && (data3['t'] <= diffTemps)){
-													console.log("OK POUR : "+i);
-												}else{
-													console.log("PAS LE TEMPS POUR : "+i);
-													elt.style.backgroundColor = "red";
-													elt.rel = "tooltip";
-													elt.title = "Attention, vous avez une resprésentation réservée pour le même jour à "+intToTime(heure1)+" qui finit à "+intToTime(fin1)+ 
-																" et il y a "+intToTime(data3['t'])+" min de trajet entre les deux salles vous n'aurez donc pas le temps d'assister aux 2 resprésentations";
-												}
-											}else{
-												var fin2 = heure2 + 120;
-												diffTemps = fin2 - heure1;
-												if((diffTemps > 0) && data3['t'] <= diffTemps){
-													console.log("OK POUR : "+i);
-												}else{
-													console.log("PAS LE TEMPS POUR : "+i);
-													elt.style.backgroundColor = "red";
-													elt.rel = "tooltip";
-													elt.title = "Attention, vous avez une resprésentation réservée pour le même jour à "+intToTime(heure2)+" qui finit à "+intToTime(fin2)+ 
-																" et il y a "+intToTime(data3['t'])+" min de trajet entre les deux salles vous n'aurez donc pas le temps d'assister aux 2 resprésentations";
-												}
-											}
-    											}
+										}
 									})
 								}
 							}
